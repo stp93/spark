0393244ffb0dd092384b3b44b35b0d4f7fdb96c5
Sam <stperry67@gmail.com>
2020-09-10 18:46:04 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, NavLink, Route } from "react-router-dom";
+import "./App.css";
+import Contact from "./Contact";
+import Home from "./Home";
+import Order from "./Order";
+import Stuff from "./Stuff";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <div className="headers">
+          <h1>SPARK</h1>
+          <h2>Order Now</h2>
+        </div>
+        <ul className="header">
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/stuff">The Cars</NavLink>
+          </li>
+          <li className="logo">
+            <NavLink exact to="/order">
+              Order
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Route exact path="/" component={Home} />
+          <Route path="/stuff" component={Stuff} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/order" component={Order} />
+        </div>
+      </div>
+    </HashRouter>
   );
 }
 
