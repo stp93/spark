e034555bd308e9502aab04fc5ff89b173b9aa80f
Sam <stperry67@gmail.com>
2020-09-10 21:31:48 -0600

    commit3

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import React from "react";
 import { HashRouter, NavLink, Route } from "react-router-dom";
 import "./App.css";
-import Contact from "./Contact";
-import Home from "./Home";
+import Custom from "./Custom";
 import Order from "./Order";
-import Stuff from "./Stuff";
+// import Stuff from "./Stuff";
 
+const Stuff = React.lazy(() => import("./Stuff"));
+const Home = React.lazy(() => import("./Home"));
 function App() {
   return (
-    <HashRouter>
-      <div className="App">
-        <div className="headers">
-          <h1>SPARK</h1>
-          <h2>Order Now</h2>
+    <React.Suspense fallback={<span>Loading...</span>}>
+      <HashRouter>
+        <div className="App">
+          <ul className="header">
+            <li>
+              <NavLink className="logo" exact to="/">
+                SPARK
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/stuff">The Cars</NavLink>
+            </li>
+            <li>
+              <NavLink exact to="/order">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/custom">Custom</NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/stuff" render={() => <Stuff />} />
+            <Route path="/custom" component={Custom} />
+            <Route path="/order" component={Order} />
+          </div>
         </div>
-        <ul className="header">
-          <li>
-            <NavLink exact to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/stuff">The Cars</NavLink>
-          </li>
-          <li className="logo">
-            <NavLink exact to="/order">
-              Order
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-        <div className="content">
-          <Route exact path="/" component={Home} />
-          <Route path="/stuff" component={Stuff} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/order" component={Order} />
-        </div>
-      </div>
-    </HashRouter>
+      </HashRouter>
+    </React.Suspense>
   );
 }
 
